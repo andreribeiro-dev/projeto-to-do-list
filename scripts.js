01bd032719a3b4e635c9a25ba334738c16582f87
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-05-01 03:34:05 -0300

    Deploy

--- scripts.js
+++ scripts.js
@@ -1,27 +1,66 @@
-let input = document.getElementById("input-principal")
-let botao = document.getElementById("botao-adicionar")
-let listaCompleta = document.getElementById("tarefas")
+const input = document.querySelector(".input-task")
+const button = document.querySelector(".button-add-task")
+const listTasks = document.querySelector('.list-tasks')
 
-let arrayDeTarefas = []
 
-function cliqueiNoBotao() {
-    arrayDeTarefas.push(input.value)
-    mostrarNaTela()
+let myToDoList = []
+
+function addNewTask() {
+    myToDoList.push({
+        task: input.value,
+        checked: false
+    })
+
+    input.value = ''
+
+    showTasks()
 }
 
-function mostrarNaTela() {
-    let novaLista = ""
+function showTasks() {
 
-    arrayDeTarefas.forEach(tarefa => {
-        novaLista = novaLista + `
-            <li>
-                <p>${tarefa}</p>
-            </li>
+    let newLi = ``
+
+    myToDoList.forEach((item, position) => {
+        newLi = newLi + `
+
+        <li class="task ${item.checked && "done"}">
+            <img src="img/checked.png" alt="check-task" onclick="checkTask(${position})">
+            <p>${item.task}</p>
+            <img src="./img/trash.png" alt="task-for-trash" onclick ="deleteTask(${position})">
+        </li>
+
         `
     })
 
-    listaCompleta.innerHTML = novaLista
+
+    listTasks.innerHTML = newLi
+
+    localStorage.setItem('list', JSON.stringify(myToDoList))
 
 }
 
-botao.addEventListener("click", cliqueiNoBotao)
+function checkTask(position) {
+    myToDoList[position].checked = !myToDoList[position].checked
+
+    showTasks()
+}
+
+function deleteTask(position) {
+    myToDoList.splice(position, 1)
+
+    showTasks()
+}
+
+function reloadTasks() {
+    const tasksLocalStorage = localStorage.getItem('list')
+
+    if (tasksLocalStorage){
+    myToDoList = JSON.parse(tasksLocalStorage)
+    }   
+    
+    showTasks()
+
+}
+
+reloadTasks()
+button.addEventListener("click", addNewTask)
